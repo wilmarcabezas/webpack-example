3984161105aaa719d3dff1c58cfe34d6b6521606
wilmarux <wilmar.cabezas@gmail.com>
2022-01-24 19:52:29 -0500

    Deploy Netlify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import Template from './templates/Template.js';
-console.log('hola');
-
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/vars.styl';
+console.log('Hello world!');
+console.log('Hello world!');
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
